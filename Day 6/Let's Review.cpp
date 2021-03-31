94fd17cced41841538ccbc8089c4a9d13da3311a
ankitkec18 <75880183+ankitkec18@users.noreply.github.com>
2021-03-31 18:10:05 +0530

    Create Let's Review.cpp

---
+++ Day 6/Let's Review.cpp
@@ -0,0 +1,24 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+   {
+       string s,s1,s2;
+       getline(cin>>ws,s);
+       for(int j=0;j<s.size();j++)
+       {
+           if(j%2==0)
+           s1+= s[j];
+           else
+           s2+= s[j];
+       }
+       cout<<s1<<" "<<s2<<endl;
+   }  
+    return 0;
+}
