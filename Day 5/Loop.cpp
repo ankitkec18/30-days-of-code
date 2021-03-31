8324486529f5943199ce48534e87f62272643541
ankitkec18 <75880183+ankitkec18@users.noreply.github.com>
2021-03-31 16:46:49 +0530

    Create Loop.cpp

---
+++ Day 5/Loop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    for(int i=1;i<=10;i++)
+    {
+       int res=n*i;
+        cout<<n<<" x "<<i<<" = "<< res <<endl;
+    }
+    return 0;
+}
